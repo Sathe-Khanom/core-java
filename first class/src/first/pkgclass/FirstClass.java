193b4b632ad0a493b272b0b973957a1825b95431
Sathe-Khanom <sathekhanom550@gmail.com>
2025-03-24 14:26:13 +0600

    add

---
+++ first class/src/first/pkgclass/FirstClass.java
@@ -0,0 +1,33 @@
+
+package first.pkgclass;
+
+
+public class FirstClass {
+
+   
+    public static void main(String[] args) {
+        byte id= 120;
+        short roll= 12342;
+        int number= 21454656;
+        long longNumber= 124325465768798789l;
+        float price= 1232.345f;
+        double doublePrice= 1234.2134;
+        
+        
+        char letter= '\u0041';
+        String name= "RahmatUllah";
+        boolean status= true;
+        
+        System.out.println("The letter is "+ letter);
+        System.out.println("His name is "+ name);
+        System.out.println("This status is "+ status);
+        System.out.println("This status is "+ price);
+        System.out.println("This status is "+ doublePrice);
+        System.out.println("This status is "+ longNumber);
+        System.out.println("This status is "+ roll);
+        System.out.println("This status is "+ id);
+        System.out.println("This status is "+ number);
+    }
+    
+  
+}

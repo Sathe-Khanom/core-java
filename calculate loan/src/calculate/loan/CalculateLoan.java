0f317073e130114d6d5a8b5f50cfc15205ae63cd
Sathe-Khanom <sathekhanom550@gmail.com>
2025-04-06 13:02:31 +0600

    add

---
+++ calculate loan/src/calculate/loan/CalculateLoan.java
@@ -0,0 +1,17 @@
+
+package calculate.loan;
+
+import java.util.Scanner;
+
+
+public class CalculateLoan {
+
+   
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        
+        
+        System.out.println("");
+    }
+    
+}

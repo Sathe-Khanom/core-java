bf7b4057f882df4ed0816f25bafcb0dec39c023f
Sathe-Khanom <sathekhanom550@gmail.com>
2025-04-07 15:08:21 +0600

    add

--- Home Work-1/src/home/work/pkg1/HomeWork1.java
+++ Home Work-1/src/home/work/pkg1/HomeWork1.java
@@ -1,18 +1,14 @@
-
 package home.work.pkg1;
 
 import java.util.Scanner;
 
-
 public class HomeWork1 {
 
-    
     public static void main(String[] args) {
-        
-//     question no-1   
 
+//     question no-1   
         Scanner s = new Scanner(System.in);
-        
+
 //        System.out.println("Enter a number to check even or odd");
 //        int number = s.nextInt();
 //        
@@ -24,10 +20,7 @@ public class HomeWork1 {
 //            System.out.println(number + " is the odd number");
 //        
 //        }
-        
-        
 //        question no-2
-
 //System.out.println("Enter a number to check positive,negative or zero(0)");
 //        int num = s.nextInt();
 //        
@@ -42,48 +35,23 @@ public class HomeWork1 {
 // else{
 //     System.out.println(num + " is zero");
 // }
- 
- 
 // question no-3
-
-System.out.println("Enter a year to check Leap Year");
+        System.out.println("Enter a year to check Leap Year");
         int year = s.nextInt();
-        
- if(year % 4 == 0){
-     if(year % 100 == 0){
-      if(year % 400 == 0){
-             System.out.println(year + " is Leap Year");}
-      else{
-        System.out.println(year + " is Leap Year");}
-  
-      }
- }
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is Leap Year");
+                } else {
+                    System.out.println(year + " not Leap Year");
+                }
+
+            } else {
+                System.out.println(year + " Leap Year");
+            }
+        } else {
+            System.out.println(year + " is not Leap Year");
         }
- 
- 
- else{
-     System.out.println(year + " is not Leap Year");
- }
- 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
-    
 }

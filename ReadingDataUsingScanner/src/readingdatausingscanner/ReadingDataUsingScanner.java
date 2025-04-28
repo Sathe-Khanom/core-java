88855a75b6674a77e2eb361f1cbb1f9d8752dd6d
Sathe-Khanom <sathekhanom550@gmail.com>
2025-04-28 18:38:08 +0600

    add

---
+++ ReadingDataUsingScanner/src/readingdatausingscanner/ReadingDataUsingScanner.java
@@ -0,0 +1,20 @@
+
+package readingdatausingscanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ReadingDataUsingScanner {
+
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        File f = new File ("C:\\Users\\Admin\\Desktop\\java.txt");
+        
+        Scanner s = new Scanner(f);
+        
+    }
+    
+}

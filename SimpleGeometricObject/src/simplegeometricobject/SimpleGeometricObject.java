7447329f9c2325192c57749eb65c0c4b42b3aaa8
Sathe-Khanom <sathekhanom550@gmail.com>
2025-05-08 18:50:50 +0600

    add

---
+++ SimpleGeometricObject/src/simplegeometricobject/SimpleGeometricObject.java
@@ -0,0 +1,13 @@
+package simplegeometricobject;
+
+public class SimpleGeometricObject {
+
+    private String color = "white";
+    private boolean filled;
+    private java.util.Date dateCreated;
+
+    public static void main(String[] args) {
+
+    }
+
+}
